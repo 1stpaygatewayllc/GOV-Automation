c841d21400ed61f518d5feb51466cab1d4d2ec3c
Imtiaz Ahmed <iahmed@govolution.com>
2020-04-03 12:40:58 -0400

    First Commit by Imtiaz

---
+++ Test Suites/QA Smoke Test/QA Smoke Test.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>QA Smoke Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>0e696436-0a54-464e-a830-790758825f6e</testSuiteGuid>
+   <testCaseLink>
+      <guid>f3b92565-d482-4429-8ae6-ecd0959032a3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/VirtualTerminal/Sale-ReqFieldsOnly</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>cf786dee-518a-43ea-86cf-df34e839fe0c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/VLink/VLinkSale-ReqFieldsOnly</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
